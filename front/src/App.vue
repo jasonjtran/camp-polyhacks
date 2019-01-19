f1dbfa34d12f2f0b948b1d534d9678b78dd013ed
Alex Rodgers <alexedrodgers@gmail.com>
2019-01-19 01:59:43 -0800

    couldn't solved the problem so I didn't

---
+++ front/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-content>
+      <gmap-map
+      style="width:1920px;height:1080px"
+      :center="center"
+      :zoom="10"
+    map-type-id="terrain"
+    >
+
+    </gmap-map>
+    </v-content>
+    
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      center: {
+        lat: 10,
+        lng: 4, 
+      },
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+ 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDf4o-SKjZ_-eSS8J01Sywo2fX9tP-klq4',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+  },
+})
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
